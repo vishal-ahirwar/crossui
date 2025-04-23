3f1bd5706bc0c07c65882ab66a5b0de559e08e57
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2025-04-23 14:35:59 +0530

    demo app added

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,45 +1,53 @@
 #include <crossui/include/App.hpp>
 #include <crossui/include/Button.hpp>
 #include <crossui/include/Rectangle.hpp>
-#include <crossui/include/Group.hpp> // New!
-
+#include <crossui/include/Group.hpp>
 #include <memory>
-#include <format>
-#include <print>
-#include <chrono>
-#include <thread>
+#include <string>
 #include <iostream>
 
-void currentDateTime()
+//CustomButton for Our Awesome App
+auto customButton(const std::string &label, int yOffset)
 {
-  using namespace std::chrono;
-  std::print("Current Date Time is : {}\n", zoned_time{current_zone(), system_clock::now()});
+  auto btn = std::make_shared<CrossUI::Button>(label);
+  btn->setSize(220, 45);
+  btn->setPosition((800 - 220) / 2, yOffset);
+  btn->setCornerRadius(18);
+  btn->setBackgroundColor({65, 64, 115, 255});
+  btn->setBorder(1, {80, 80, 80, 255});
+  return btn;
 }
-
 int main()
 {
-  CrossUI::App app("CrossUI Hello", 800, 600);
+  CrossUI::App app("CrossUI | Main Menu", 800, 600);
 
-  auto group = std::make_shared<CrossUI::Group>();
+  // Root group container
+  auto root = std::make_shared<CrossUI::Group>();
 
-  auto btn = std::make_shared<CrossUI::Button>("Print");
-  btn->setPosition(350, 250);
-  btn->setSize(190, 45);
-  btn->setBorder(1, {255, 255, 255, 255});
-  btn->onClick(&currentDateTime);
-
+  // Create Panel
   auto panel = std::make_shared<CrossUI::Rectangle>();
-  panel->setPosition(320, 200);
-  panel->setSize(600, 400);
-  panel->setCornerRadius(12);
-  panel->setBackgroundColor({40, 40, 40, 255});
-  panel->setBorder(2, {0, 255, 255, 255});
+  int panelWidth = 300;
+  int panelHeight = 300;
+  panel->setPosition((800 - panelWidth) / 2, (600 - panelHeight) / 2);
+  panel->setSize(panelWidth, panelHeight);
+  panel->setCornerRadius(18);
+  panel->setBackgroundColor({30, 30, 30, 255});
+  panel->setBorder(2, {80, 80, 80, 255});
+  auto loginBtn = customButton("Signin", 200);
+  auto createBtn = customButton("Signup", 270);
+  auto guestBtn = customButton("Guest", 340);
 
-  // Add both to group
-  group->addChild(panel);
-  group->addChild(btn);
+  // Add callbacks
+  loginBtn->onClick([]{ std::cout << "Login clicked\n"; });
+  createBtn->onClick([]{ std::cout << "Create clicked\n"; });
+  guestBtn->onClick([]{ std::cout << "Guest clicked\n"; });
 
-  app.setRootWidget(group);
+  // Add all to root group
+  root->addChild(panel);
+  root->addChild(loginBtn);
+  root->addChild(createBtn);
+  root->addChild(guestBtn);
 
+  app.setRootWidget(root);
   return app.run();
 }
